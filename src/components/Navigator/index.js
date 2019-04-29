5df526f6214e96523d7da5a0050264a2daf963f8
huyngopt1994 <huyngopt1994@gmail.com>
2019-04-29 12:07:20 +0700

    Init routing and structure folders

---
+++ src/components/Navigator/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Navigator extends Component {
+    render() {
+        return (
+            <div>
+                This is Navigator
+            </div>
+        )
+    }
+}
+
+export default Navigator
