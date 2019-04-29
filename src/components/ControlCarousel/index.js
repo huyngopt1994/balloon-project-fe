99f634c03a126702c0380284c830c43f8029d1e8
huyngopt1994 <huyngopt1994@gmail.com>
2019-04-29 16:21:15 +0700

    Remove some unused libs and add the first coursela

---
+++ src/components/ControlCarousel/index.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import Carousel from 'react-bootstrap/Carousel'
+import slide_1 from '../../assets/images/slide-1.jpeg'
+import slide_3 from '../../assets/images/slide-3.jpeg'
+
+class ControlledCarousel extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+
+        this.handleSelect = this.handleSelect.bind(this);
+
+        this.state = {
+            index: 0,
+            direction: null,
+        };
+    }
+
+    handleSelect(selectedIndex, e) {
+        this.setState({
+            index: selectedIndex,
+            direction: e.direction,
+        });
+    }
+
+    render() {
+        const { index, direction } = this.state;
+
+        return (
+            <Carousel
+                // activeIndex={index}
+                // direction={direction}
+                // onSelect={this.handleSelect}
+            >
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={slide_1}
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <h3>First slide label</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={slide_3}
+                        alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Second slide label</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+        );
+    }
+}
+
+export default ControlledCarousel;
--- src/modules/Homes/index.js
+++ src/modules/Homes/index.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import ControlledCarousel from '../../components/ControlCarousel'
 
 class Home extends Component {
     render() {
         return (
             <div>
                 This is Home
+                <ControlledCarousel/>
             </div>
+
         )
     }
 }
