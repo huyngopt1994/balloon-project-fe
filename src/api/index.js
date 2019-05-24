8497438a2a3146e26b4c84c57d961501ed41d25a
huyngopt1994 <huyngopt1994@gmail.com>
2019-05-24 22:07:40 +0700

    Add Api file

---
+++ src/api/index.js
@@ -0,0 +1,47 @@
+function getProductList() {
+    return
+}
+
+function getProductOne() {
+    return
+}
+
+function createProduct() {
+    return
+}
+
+function updateProduct() {
+
+}
+
+function getCompanyList() {
+    return
+}
+
+function getCompanyOne() {
+    return
+}
+
+function createCompany() {
+    return
+}
+
+function updateCompany() {
+    return
+}
+
+function getTransactionList() {
+    return
+}
+
+function getTransactionOne() {
+    return
+}
+
+function createTransaction() {
+    return
+}
+
+function updateTransaction() {
+    return
+}
